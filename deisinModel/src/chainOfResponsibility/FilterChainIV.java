c34b7c82b9401d6a1f74f3c9e7ab9a60425524ab
fuzekun <1078682405@qq.con>
2022-05-15 21:07:43 +0800

    责任链模式的讲解

--- deisinModel/src/chainOfResponsibility/FilterChainIV.java
+++ deisinModel/src/chainOfResponsibility/FilterChainIV.java
@@ -18,6 +18,9 @@ public class FilterChainIV {
         fc.doFilter(req, res, fc);
         System.out.println("req = " + req.msg);
         System.out.println("resp = " + res.msg);
+        fc.doFilter(req, res, fc);
+        System.out.println("req = " + req.msg);
+        System.out.println("resp = " + res.msg);
     }
 }
 
@@ -30,7 +33,7 @@ class MyResponse {
 }
 
 interface MyFilter {
-    public void doFilter(MyRequest req, MyResponse resp, MyFilterChain fc);
+    public void doFilter(MyRequest req, MyResponse resp, MyFilter next);
 }
 
 class MyFilterChain implements MyFilter{
@@ -43,28 +46,29 @@ class MyFilterChain implements MyFilter{
     }
 
     @Override
-    public void doFilter(MyRequest req, MyResponse resp, MyFilterChain fc) {
+    public void doFilter(MyRequest req, MyResponse resp, MyFilter next) {
         if (index >= filters.size()) return ;
         MyFilter filter = filters.get(index);
         index++;
-        filter.doFilter(req, resp, fc);
+        filter.doFilter(req, resp, next);
+        index--;                    // 进行回溯，否则没法进行重新过滤了
     }
 }
 
 class MyHtmlFilter implements MyFilter{
 
     @Override
-    public void doFilter(MyRequest req, MyResponse resp, MyFilterChain fc) {
+    public void doFilter(MyRequest req, MyResponse resp,  MyFilter next) {
         req.msg += "Req.htmlFilter-->";
-        fc.doFilter(req, resp, fc);           // 如果从这里不进行递归调用，就不进行下一步处理了
+        next.doFilter(req, resp, next);           // 如果从这里不进行递归调用，就不进行下一步处理了
         resp.msg += "Resp.htmlFilter-->";
     }
 }
 class MySensitiveFilter implements MyFilter{
     @Override
-    public void doFilter(MyRequest req, MyResponse resp, MyFilterChain fc) {
+    public void doFilter(MyRequest req, MyResponse resp, MyFilter next) {
         req.msg += "Req.sensitiveFilter-->";
-        fc.doFilter(req, resp, fc);             // 有用户自己决定是否进行下一步的调用
+        next.doFilter(req, resp, next);             // 有用户自己决定是否进行下一步的调用
         resp.msg += "Resp.sensitiveFilter-->";
     }
 }
